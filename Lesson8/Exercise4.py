7611247767f48afb7960ee05ec2b69bb505ba237
Anbionchik <Anbion@yandex.ru>
2022-01-24 19:45:00 +0300

    Доделано домашнее задание по уроку 8

--- Lesson8/Exercise4.py
+++ Lesson8/Exercise4.py
@@ -11,55 +11,74 @@
 # для указания количества принтеров, отправленных на склад, нельзя использовать строковый тип данных.
 # Подсказка: постарайтесь реализовать в проекте «Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
 
-from random import randint
 
+class NotAnOfficeEquipment(Exception):
+    def __init__(self, error_text):
+        self.error_text = error_text
+    
 
 class WareHouse:
     def __init__(self):
-        self.stock = {}
-        self.id_list = []
-
-    def check_presence(self, unit):
-        if unit.type not in self.stock:
+        self.__stock = {}
+        
+    @staticmethod
+    def is_office_equipment(unit):
+        if not isinstance(unit, OfficeEquipment):
             return False
         else:
-            if unit not in self.stock[unit.type]:
+            return True
+
+    def check_presence(self, unit):
+        if unit.type not in self.__stock:
+            return False
+        else:
+            if unit not in self.__stock[unit.type]:
                 return False
             else:
                 return True
 
     def receive_unit(self, unit):
-        # unit._id = self.create_id()
-        # self.stock[unit._id] = unit
-        # print("{} зачислен на склад под идентификатором {}.".format(unit, unit._id))
+        try:
+            if not isinstance(unit, OfficeEquipment):
+                raise NotAnOfficeEquipment("Данный объект не является офисным оборудованием.")
+        except NotAnOfficeEquipment as e:
+            print(e)
+            return
+        
         if self.check_presence(unit):
             print("Такой объект уже числится на складе.")
             return
 
-        if unit.type not in self.stock:
-            self.stock[unit.type] = [unit]
+        if unit.type not in self.__stock:
+            self.__stock[unit.type] = [unit]
             print(f"{unit} успешно зачислен на склад.")
         else:
-            self.stock[unit.type].append(unit)
+            self.__stock[unit.type].append(unit)
             print(f"{unit} успешно зачислен на склад.")
-
+            
     def move_unit(self, unit, destination):
         if not self.check_presence(unit):
             print("На складе такого объекта нет.")
             return
-        self.stock[unit.type].remove(unit)
+        self.__stock[unit.type].remove(unit)
         print("{} перемещён в отдел {}".format(unit, destination))
-
-    # def create_id(self):
-    #     while True:
-    #         new_id = ''.join(["{}".format(randint(0, 9)) for _ in range(0, 6)])
-    #         if not new_id in self.id_map:
-    #             return new_id
+        
+    def show_stock(self, units_type=None):
+        if units_type is not None:
+            if units_type in self.__stock:                
+                for unit in self.__stock[units_type]:
+                    print(unit)
+            else:
+                print("Оборудования такого типа на складе нет.")
+        else:
+            for unit_type, units_list in self.__stock.items():
+                print(f'Тип оборудования {unit_type}:')
+                for unit in units_list:
+                    print(unit)
 
 
 class OfficeEquipment:
     def __init__(self, manufacturer, model):
-        # self._id = None
         self.manufacturer = manufacturer
         self.model = model
 
@@ -72,6 +91,10 @@ class Printer(OfficeEquipment):
     def __str__(self):
         return "Принтер {} {}".format(self.manufacturer, self.model)
 
+    @staticmethod
+    def print_obj(obj):
+        print(f"{obj} успешно напечатан.")
+
 
 class Scanner(OfficeEquipment):
     def __init__(self, manufacturer, model):
@@ -81,6 +104,10 @@ class Scanner(OfficeEquipment):
     def __str__(self):
         return "Сканнер {} {}".format(self.manufacturer, self.model)
 
+    @staticmethod
+    def scan(obj):
+        print(f"{obj} успешно просканирован.")
+
 
 class MFU(OfficeEquipment):
     def __init__(self, manufacturer, model):
@@ -89,6 +116,18 @@ class MFU(OfficeEquipment):
 
     def __str__(self):
         return "МФУ {} {}".format(self.manufacturer, self.model)
+
+    @staticmethod
+    def print_obj(obj):
+        print(f"{obj} успешно напечатан.")
+
+    @staticmethod
+    def scan(obj):
+        print(f"{obj} успешно просканирован.")
+    
+    def copy(self, obj):
+        self.scan(obj)
+        self.print_obj(obj)
 
 
 new_warehouse = WareHouse()
@@ -103,6 +142,15 @@ MFU2 = MFU("Canon", "Foo")
 MFU3 = MFU("Xerox", "3500")
 
 new_warehouse.receive_unit(printer2)
-new_warehouse.receive_unit(printer2)
+new_warehouse.receive_unit(printer1)
+new_warehouse.receive_unit(printer3)
+new_warehouse.receive_unit(scanner1)
+new_warehouse.receive_unit(scanner2)
+new_warehouse.receive_unit(scanner3)
+new_warehouse.receive_unit(MFU3)
+new_warehouse.receive_unit(MFU1)
+new_warehouse.receive_unit(MFU2)
+new_warehouse.receive_unit("sdfasdf")
 new_warehouse.move_unit(printer2, "Офис на втором этаже")
+
 
