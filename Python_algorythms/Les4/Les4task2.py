f85878de2c11b6a44283ce01ed6e86cb1f5b95c1
Anbionchik <Anbion@yandex.ru>
2022-05-30 21:25:55 +0300

    Сделано задание 2 урока 4

--- Python_algorythms/Les4/Les4task2.py
+++ Python_algorythms/Les4/Les4task2.py
@@ -1,4 +1,52 @@
+import cProfile
+
+def sieve(n):
 
 
-def eratosfen(n):
-    pass
+    primes_list = [2]
+    number = 3
+
+    while len(primes_list) < n:
+        skip = False
+        for i in primes_list:
+            if not number % i:
+                skip = True
+                break
+        if skip:
+            number += 1
+            continue
+        else:
+            for i in range(primes_list[-1], int(number**0.5)):
+                if not number % i:
+                    skip = True
+                    break
+        if skip:
+            number += 1
+            continue
+        else:
+            primes_list.append(number)
+
+    return primes_list[-1]
+
+
+def primes(n):
+
+    primes_list = []
+    number = 2
+    while len(primes_list) < n:
+        complex = False
+        for i in range(2, number):
+            if not number % i:
+                complex = True
+                break
+        if not complex:
+            primes_list.append(number)
+        number +=1
+
+    return primes_list[-1]
+
+
+# cProfile.run('primes(100)')
+# print(sieve(2000))
+# print(primes(2000))
+
