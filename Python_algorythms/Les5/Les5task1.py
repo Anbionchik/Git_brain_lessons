bad6a30d64370baed024b1d0d3c3f589172cc57a
Anbionchik <Anbion@yandex.ru>
2022-06-02 20:32:37 +0300

    Задание 1 урока 5 готово

---
+++ Python_algorythms/Les5/Les5task1.py
@@ -0,0 +1,30 @@
+from collections import namedtuple, OrderedDict
+from statistics import mean
+
+new_company = namedtuple("new_company", "name, profit_1, profit_2, profit_3, profit_4")
+companies_list = []
+
+while True:
+    input_list = input("Введите через пробел наименование компании и доход за 4 квартала. "
+                       "Для завершения не вводите ничего: ").split(' ', 4)
+    if not input_list[0]:
+        break
+    input_list = input_list[0:1] + list(map(int, input_list[1:]))
+# input_list = ['комп1', 213, 13153, 351, 135]
+    companies_list.append(new_company(*input_list))
+
+
+
+if companies_list:
+    profit_dict = {}
+    for i in companies_list:
+        profit_dict[i.name] = sum([i.profit_1, i.profit_2, i.profit_3, i.profit_4])
+    profit = OrderedDict(sorted(profit_dict.items(), key=lambda x: x[1]))
+
+    print('Наименьших доход в размере {a[1]} был получен компанией {a[0]}'.format(a = profit.copy().popitem(last=False)))
+    print('Наибольший доход в размере {a[1]} был получен компанией {a[0]}'.format(a = profit.copy().popitem(last=True)))
+    print(f'Средний доход: {mean(profit.values())}')
+
+
+
+
